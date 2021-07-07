57515b312faa0bf65cf41702a6d6608afdbb1cf5
Roberto Oliveros <robertooliveros@mac.com>
2021-07-07 13:28:10 -0600

    Style text

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  h1: {
+    color: '#0077cc',
+    fontSize: 48,
+    fontWeight: 'bold'
+  },
+  paragraph: {
+    marginTop: 24,
+    marginBottom: 24,
+    fontSize: 18
+  }
+});
 
 const Main = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello world!</Text>
+    <View style={styles.container}>
+      <Text style={styles.h1}>
+        Hello world!
+      </Text>
+      <Text styles={styles.paragraph}>
+        This is my app
+      </Text>
       <Image source={require('../assets/images/hello-world.jpg')} />
     </View>
   );
