ce7bf157b1e56f86d8ecc387c95908be3b0c29ba
Roberto Oliveros <robertooliveros@mac.com>
2021-07-08 12:20:31 -0600

    Use styled components

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  h1: {
-    color: '#0077cc',
-    fontSize: 48,
-    fontWeight: 'bold'
-  },
-  paragraph: {
-    marginTop: 24,
-    marginBottom: 24,
-    fontSize: 18
-  }
-});
+const StyledView = styled.View`
+  flex: 1;
+  justify-content: center;
+`;
+
+const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const P = styled.Text`
+  margin: 24px 0;
+  font-size: 18px;
+`;
 
 const Main = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>
-        Hello world!
-      </Text>
-      <Text styles={styles.paragraph}>
-        This is my app
-      </Text>
+    <StyledView>
+      <H1>Hello Sebas!</H1>
+      <P>This is my app</P>
       <Image source={require('../assets/images/hello-world.jpg')} />
-    </View>
+    </StyledView>
   );
 };
 
